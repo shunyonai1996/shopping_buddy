827dcaa969ff425514c001c5c42e7f92a1718455
yonai <yonai@crudist.jp>
2023-04-29 13:43:01 +0900

    add typescript vuex

---
+++ frontend/src/store/index.ts
@@ -0,0 +1,27 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    count: 0,
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    },
+  },
+  actions: {
+    increment(context) {
+      context.commit('increment');
+    },
+  },
+  getters: {
+    count(state) {
+      return state.count;
+    },
+  },
+});
+
+export default store;
